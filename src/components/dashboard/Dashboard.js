04f821978cf7ad560210cf05d3b58376cb3840be
Sohanur <sohanur471@gmail.com>
2022-04-05 10:40:54 +0600

    investment vs revenue

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import InvestmentVsRevenue from "./InvestmentVsRevenue";
 import MonthlySell from "./MonthlySell";
 
 const Dashboard = () => {
@@ -11,13 +12,14 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <div className="container-fluid pt-5 pb-5 d-flex flex-row justify-content-between">
+    <div className="container-fluid pt-5 pb-5 d-flex flex-column flex-md-row justify-content-evenly">
       <div>
         <div className="h4 p-2 text-primary">Month Wise Sell</div>
         <MonthlySell data={data}></MonthlySell>
       </div>
       <div>
-          
+        <div className="h4 p-2 text-primary">Investment Vs Revenue</div>
+        <InvestmentVsRevenue data={data}></InvestmentVsRevenue>
       </div>
     </div>
   );
--- src/components/dashboard/InvestmentVsRevenue.js
+++ src/components/dashboard/InvestmentVsRevenue.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React from "react";
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
-const InvestmentVsRevenue = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const InvestmentVsRevenue = ({ data }) => {
+  return (
+    <AreaChart
+      width={500}
+      height={300}
+      data={data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="month" />
+      <YAxis />
+      <Tooltip />
+      <Area
+        type="monotone"
+        dataKey="revenue"
+        stackId="1"
+        stroke="#8884d8"
+        fill="#8884d8"
+      />
+      <Area
+        type="monotone"
+        dataKey="investment"
+        stackId="1"
+        stroke="#82ca9d"
+        fill="#82ca9d"
+      />
+    </AreaChart>
+  );
 };
 
 export default InvestmentVsRevenue;
