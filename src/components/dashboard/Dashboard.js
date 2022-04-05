85a57371c5f1a4ef143fecc67de9fce36048765a
Sohanur <sohanur471@gmail.com>
2022-04-05 10:10:41 +0600

    Line chart

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import MonthlySell from "./MonthlySell";
 
 const Dashboard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("data.json")
+      .then((response) => response.json())
+      .then((responseData) => setData(responseData));
+  }, []);
+
+  return (
+    <div className="container-fluid pt-5 pb-5 d-flex flex-row justify-content-between">
+      <div>
+        <div className="h4 p-2 text-primary">Month Wise Sell</div>
+        <MonthlySell data={data}></MonthlySell>
+      </div>
+      <div>
+          
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
---
+++ src/components/dashboard/MonthlySell.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+const MonthlySell = ({ data }) => {
+  return (
+    <LineChart
+      width={500}
+      height={300}
+      data={data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="month" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line
+        type="monotone"
+        dataKey="sell"
+        stroke="#8884d8"
+        activeDot={{ r: 8 }}
+      />
+    </LineChart>
+  );
+};
+
+export default MonthlySell;
